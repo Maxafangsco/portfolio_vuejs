3796077f63dac7787c5356e7e274d1b0c4586c5a
maxafangsco <maxafangsco@gmail.com>
2021-07-15 15:58:45 +0400

    this is a complete version of vue 3 portfolio

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/css/styles.css'
+import './assets/css/bootstrap.css'
+
+//Vue.config.productionTip = false
+
+createApp(App).mount('#app')
